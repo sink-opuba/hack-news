ca9fe749a147df165bbc35b126e6f587cea9257c
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2019-12-09 12:29:10 +0100

    commbine archive and story reducers in redux store

--- src/index.js
+++ src/index.js
@@ -2,29 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
+import store from "./store";
 import * as serviceWorker from "./serviceWorker";
 
-const stories = [
-  {
-    title: "React",
-    url: "http://facebook.github.io/react/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "http://github.com/reactjs/redux",
-    author: "Dan Abramov, Andrew Clarke",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
 ReactDOM.render(
   <App
-    stories={stories}
+    stories={store.getState().storyState}
     onArchive={id => {
       console.log(id);
     }}
