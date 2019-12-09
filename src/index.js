e31447b54671e553b82780299d51a2a93a1f2a3d
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2019-12-09 14:06:25 +0100

    implement react re-render on redux store update

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 import store from "./store";
+import { getReadableStories } from "./selectors/story";
+import { STORY_ARCHIVE } from "./constants/actionTypes";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <App
-    stories={store.getState().storyState}
-    onArchive={id => {
-      console.log(id);
-    }}
-  />,
-  document.getElementById("root")
-);
+function render() {
+  ReactDOM.render(
+    <App
+      stories={getReadableStories(store.getState())}
+      onArchive={id => {
+        store.dispatch({ type: STORY_ARCHIVE, id });
+      }}
+    />,
+    document.getElementById("root")
+  );
+}
 
+store.subscribe(render);
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
